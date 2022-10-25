f40d76cf7e16262545cb474133901dd5ac52535e
Kerim <86295374+Kerim-tk@users.noreply.github.com>
2022-10-25 13:21:15 +0400

    1

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function dividing(){
+    const a = +prompt ('please enter the value of A:');
+    const b = +prompt ('please enter the value of B:');
+    
+    console.log(a);
+    console.log(b);
+    
+    let result = a / b;
+    let remainder = a % b;
+    console.log("result of dividing:", result);
+    console.log("remainder of the division", remainder);
+}
+    
+
+
+
+
+dividing ()
